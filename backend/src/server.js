8dbee311054b380b00dd08136de336eb30ca7412
Nischal Dhungana <nischaldhungana10@gmail.com>
2025-12-19 16:01:20 +0545

    Ready for Deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,16 @@
 import express from "express"
 import dotenv from "dotenv"
+import path from "path";
+
 import authRouter from "./routes/auth.route.js";
 import messageRouter from "./routes/message.route.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-
-
 const app = express()
+
+const __dirname = path.resolve();
 
 
 
@@ -16,9 +18,13 @@ const app = express()
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
 
-app.get("/",(req,res)=>{
-    res.send("Hello");
-})
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    app.get("*", (req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
+    })
+}
 
 
 
