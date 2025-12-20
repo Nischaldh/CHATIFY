f3abe27cd220f3c009639fa83fc161c9bcdad2e3
Nischal Dhungana <nischaldhungana10@gmail.com>
2025-12-20 19:46:01 +0545

    Create Update Profile Route

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import bcrypt from "bcryptjs"
 import { generateToken } from "../lib/utlis.js";
 import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 import { ENV } from "../lib/env.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res)=>{
     const {fullName, email , password} = req.body;
@@ -55,11 +56,9 @@ export const signup = async (req, res)=>{
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
-
   if (!email || !password) {
     return res.status(400).json({ success:false, message: "Email and password are required" });
   }
-
   try {
     const user = await User.findOne({ email });
     const hashToCompare = user ? user.password : ENV.DUMMY_HASH;
@@ -91,3 +90,21 @@ export const logout = (_, res) => {
   res.cookie("jwt", "", { maxAge: 0 });
   res.status(200).json({ message: "Logged out successfully" });
 };
+
+export const updateProfile = async(req, res)=>{
+    try {
+        const {profilePic} = req.body;
+        if(!profilePic) return res.status(400).json({success:false, message:"Profile Pic is required"});
+        const userId = req.user._id;
+
+       const uploadResponse = await cloudinary.uploader.upload(profilePic);
+       const updatedUser= await User.findByIdAndUpdate(userId, {profilePic:uploadResponse.secure_url}, {new:true})
+
+       res.status(200).json({success:true, message:"Profile Updated Successfully"})
+
+    } catch (error) {
+        console.error("Error in profile Update:", error);
+        res.status(500).json({ message: "Internal server error" });   
+    }
+}
+
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import { ENV } from "./lib/env.js";
 import path from "path";
+import cookieParser from "cookie-parser"
 
 import authRouter from "./routes/auth.route.js";
 import messageRouter from "./routes/message.route.js";
@@ -12,6 +13,7 @@ const PORT = ENV.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cookieParser())
 
 const __dirname = path.resolve();
 
