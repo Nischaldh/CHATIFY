c39d1f9eb495ea71b93089df93addbaa80e13f9d
Nischal Dhungana <nischaldhungana10@gmail.com>
2025-12-21 16:18:47 +0545

    Apply rate limiting

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,15 +1,16 @@
 import express from "express";
 import { login, logout, signup,  updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjectProtection } from "../middleware/arcjet.middleware.js";
 
 const authRouter  = express.Router();
 
+authRouter.use(arcjectProtection);
 
-authRouter.post("/signup", signup);
-authRouter.post("/login", login);
-authRouter.post("/logout", logout);
-authRouter.put("/update-profile", protectRoute,updateProfile)
-authRouter.get("/check", protectRoute, (req, res)=>res.status(200).json(req.user));
+authRouter.post("/signup",signup);
+authRouter.post("/login",login);
+authRouter.post("/logout",logout);
+authRouter.put("/update-profile",protectRoute,updateProfile)
 
 
 export default authRouter;
