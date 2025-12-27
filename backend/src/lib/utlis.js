86e16e820e5c38476c2aad23c54c7e9c1ea2da66
Nischal Dhungana <nischaldhungana10@gmail.com>
2025-12-27 12:25:57 +0545

    Fix production issue

--- backend/src/lib/utlis.js
+++ backend/src/lib/utlis.js
@@ -9,7 +9,7 @@ export const generateToken = (userId, res)=>{
     res.cookie("jwt", token, {
         maxAge: 7 * 24* 60 *60 *1000,
         httpOnly:true, //for XSS
-        sameSite: "strict", // for CSRF
+        sameSite: "none", // for CSRF
         secure:process.env.NODE_ENV ==="development"?false:true,
     })
     return token;
